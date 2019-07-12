b56d716d80a5df3de0cd71c96d85ccfc1db53ee0
Omar Dulaimi <omar@eila.io>
2019-07-12 18:48:31 +0300

    refactor+cleanup notes service

--- frontend/src/services/notes.service.js
+++ frontend/src/services/notes.service.js
@@ -1,10 +1,10 @@
-exports.baseUrl = 'http://localhost:3000/api/v1';
+const axios = require('../../../api/services/axios.service');
 
 exports.getAllNotes = async () => {
     try {
-        const res = await fetch(`${this.baseUrl}/notes`);
+        const res = await axios.get('/notes');
         const visible = true;
-        const notes = await res.json();
+        const notes = res.data;
         return [notes, notes[0], visible];
     } catch (e) {
         console.error(e);
@@ -14,16 +14,8 @@ exports.getAllNotes = async () => {
 
 exports.updateNote = async (note, id) => {
     try {
-        const res = await fetch(`${this.baseUrl}/notes/${id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(note),
-        });
-
-        let response = await res.json();
-        return [response, true];
+        const res = await axios.patch(`/notes/${id}`, note);
+        return [res, true];
     } catch (e) {
         console.error(e);
         return [null, false];
@@ -32,12 +24,8 @@ exports.updateNote = async (note, id) => {
 
 exports.deleteNote = async (id) => {
     try {
-        const res = await fetch(`${this.baseUrl}/notes/${id}`, {
-            method: 'DELETE',
-        });
-
-        let response = await res.json();
-        return [response, true];
+        const res = await axios.delete(`/notes/${id}`);
+        return [res, true];
     } catch (e) {
         console.error(e);
         return [null, false];
@@ -46,16 +34,8 @@ exports.deleteNote = async (id) => {
 
 exports.createNote = async (note) => {
     try {
-        const res = await fetch(`${this.baseUrl}/notes`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(note),
-        });
-
-        let response = await res.json();
-        return [response, true];
+        const res = await axios.post(`/notes`, note);
+        return [res, true];
     } catch (e) {
         console.error(e);
         return [null, false];
