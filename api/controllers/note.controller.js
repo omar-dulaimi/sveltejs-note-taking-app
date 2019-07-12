f44ad72e0890ebc5837e5d5a14be452a9294af0b
Omar Dulaimi <omar@eila.io>
2019-07-12 18:42:04 +0300

    implement note controller crud operations

--- api/controllers/note.controller.js
+++ api/controllers/note.controller.js
@@ -1,3 +1,40 @@
-exports.list = (req, res) => {
-    res.send('It works!');
+const Note = require('../models/note.model');
+
+exports.getAllNotes = async (req, res) => {
+    try {
+        const notes = await Note.find({ isDeleted: { $ne: true } });
+        res.json(notes);
+    } catch (e) {
+        res.status(404).json({ msg: 'Error getting notes', error: e });
+    }
+};
+
+exports.createNote = async (req, res) => {
+    try {
+        const note = new Note(req.body);
+        await note.save();
+        res.json(note);
+    } catch (e) {
+        res.status(404).json({ msg: 'Error creating note', error: e });
+    }
+};
+
+exports.updateNote = async (req, res) => {
+    try {
+        const noteId = req.params.id;
+        const note = await Note.findByIdAndUpdate(noteId, req.body);
+        res.json(note);
+    } catch (e) {
+        res.status(404).json({ msg: 'Error updating note', error: e });
+    }
+};
+
+exports.deleteNote = async (req, res) => {
+    try {
+        const noteId = req.params.id;
+        const note = await Note.findByIdAndUpdate(noteId, { isDeleted: true });
+        res.json(note);
+    } catch (e) {
+        res.status(404).json({ msg: 'Error deleting note', error: e });
+    }
 };
