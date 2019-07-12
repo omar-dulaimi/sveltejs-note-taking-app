07892a6b1e9bf904e4e9c889ed1ca6956682889a
Omar Dulaimi <omar@eila.io>
2019-07-12 18:44:39 +0300

    refactor note routes

--- api/routes/v1/note.route.js
+++ api/routes/v1/note.route.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const NoteController = require('../../controllers/note.controller');
 
-router.route('/').get(NoteController.list);
+router
+    .route('/')
+    .get(NoteController.getAllNotes)
+    .post(NoteController.createNote);
+
+router
+    .route('/:id')
+    .patch(NoteController.updateNote)
+    .delete(NoteController.deleteNote);
 
 module.exports = router;
